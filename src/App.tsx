51f445d7f68253dcd3aed5311f20b099dfd11dbd
Nikita Sukhachev <traed.traed@mail.ru>
2022-09-19 17:25:41 +0700

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+
+import './styles.scss';
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='container'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Navigate to='/discover' replace />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header/NavLink.tsx
@@ -0,0 +1,24 @@
+import { Link, useMatch } from 'react-router-dom';
+import cn from 'classnames';
+
+import styles from './styles.module.scss';
+
+interface INavLink {
+  to: string;
+  children: React.ReactNode;
+}
+
+const NavLink: React.FC<INavLink> = ({ to, children }) => {
+  const isMatching = useMatch(to);
+
+  return (
+    <Link
+      to={to}
+      className={cn(styles.navLink, { [styles.navLink_match]: isMatching })}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export { NavLink };
